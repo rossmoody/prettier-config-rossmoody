ec0dbbe5521b7a4b621a27e42df924473570379d
rossmoody <fiftyfivehis@gmail.com>
2021-04-29 18:48:42 -0700

    initiall commit

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 4,
+  useTabs: true,
+  semi: false,
+  singleQuote: true,
+  // quoteProps: "as-needed",
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  // bracketSpacing: true,
+  // jsxBracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'never'
+  // htmlWhitespaceSensitivity: "css",
+  // vueIndentScriptAndStyle: false,
+  // endOfLine: "auto",
+}
