6a2999c1f7e1ddfeeeb3b05c8c3f65f1021c0221
rossmoody <fiftyfivehis@gmail.com>
2021-04-29 22:14:06 -0700

    initial

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 module.exports = {
-  printWidth: 80,
-  tabWidth: 4,
-  useTabs: true,
+  trailingComma: "es5",
+  tabWidth: 2,
   semi: false,
   singleQuote: true,
-  // quoteProps: "as-needed",
-  jsxSingleQuote: true,
-  trailingComma: 'es5',
-  // bracketSpacing: true,
-  // jsxBracketSameLine: false,
-  arrowParens: 'always',
-  proseWrap: 'never'
-  // htmlWhitespaceSensitivity: "css",
-  // vueIndentScriptAndStyle: false,
-  // endOfLine: "auto",
-}
+};
